d827a1469ad4c7c5808840db4ecded3244ae5352
hi-hot-day <70693975+hi-hot-day@users.noreply.github.com>
2020-09-09 16:06:22 +0800

    Update jump7.py

--- jump7.py
+++ jump7.py
@@ -1,5 +1,5 @@
-for a in range(1,101):
-    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7:
+for a in range(1,101): # a大于等于1，小于101
+    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7: # 如果 a 可以被7整除，除以10取余为7，除以10取整为7，则跳过，否则打印a
         continue
     else:
         print(a)
